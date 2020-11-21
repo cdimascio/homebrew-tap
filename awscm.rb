d23b00acc6deedbc4c62f1b1dc2e992b4b19cad4
Carmine DiMascio <cdimascio@gmail.com>
2020-11-20 20:23:54 -0500

    initial version

--- awscm.rb
+++ awscm.rb
@@ -1,7 +1,7 @@
 class HomebrewAwsCredentialsManager < Formula
   desc "manage and switch between multiple ~/.aws/credentials files"
   homepage "http://github.com/cdimascio/homebrew-aws-credentials-manager"
-  url "https://github.com/cdimascio/homebrew-aws-credentials-manager/archive/v0.9.0.tar.gz"
+  url "https://github.com/cdimascio/homebrew-aws-credentials-manager/archive/v0.9.1.tar.gz"
   version "0.9.0"
   # sha256 "3cecb3fe5e116c7427370cfd4d1b7f7c47d4482969d417126dc7c78ebf56ff18"
   license "MIT"
