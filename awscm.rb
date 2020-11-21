952d9949c75a513b6de79c29f2b89bee9aa3c27d
Carmine DiMascio <cdimascio@gmail.com>
2020-11-20 20:36:11 -0500

    initial version

--- awscm.rb
+++ awscm.rb
@@ -2,7 +2,7 @@ class AwsCredentialsManager < Formula
   desc "Manage multiple AWS credentials files e.g. ~/.aws/credentials"
   homepage "https://github.com/cdimascio/aws-credentials-manager"
   url "https://github.com/cdimascio/aws-credentials-manager/archive/v0.9.2.tar.gz"
-  sha256 "137083543278ced4f69dedf870f7bcb0ece04d01be8300b672ca3168fbdcfec3"
+  sha256 "f8611273829c14782598472ca5228b399f12faf8bec3f476b0d19b44df2f0644"
   license "MIT"
 
   depends_on "bash" => :run
@@ -14,3 +14,4 @@ class AwsCredentialsManager < Formula
   test do
   end
 end
+
