ecdae00c2fa076a3c6494239dfbc127007286c16
Carmine DiMascio <cdimascio@gmail.com>
2020-11-22 21:16:36 -0500

    v1.0.0

--- Formula/aws_credentials_manager.rb
+++ Formula/aws_credentials_manager.rb
@@ -1,8 +1,8 @@
 class AwsCredentialsManager < Formula
   desc "Manage multiple AWS credentials files e.g. ~/.aws/credentials"
   homepage "https://github.com/cdimascio/aws-credentials-manager"
-  url "https://github.com/cdimascio/aws-credentials-manager/archive/v0.10.0.tar.gz"
-  sha256 "592241b2c1efe398b3b3f473e43704cde934ed203d0b66e2ef3cd87b00f88bf6"
+  url "https://github.com/cdimascio/aws-credentials-manager/archive/v1.0.0.tar.gz"
+  sha256 "9730dec73a50f80b5d841fb041c9d2ce6b89e08c049c8afc12a9a4ef982957a0"
   license "MIT"
 
   depends_on "bash"
