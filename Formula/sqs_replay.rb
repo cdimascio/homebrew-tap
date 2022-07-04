e591e864cc6ac18f4c312363baab068dbb913bb0
Carmine DiMascio <cdimasci@amazon.com>
2022-07-04 12:36:15 -0400

    sqs-replay v0.12.0

--- Formula/sqs_replay.rb
+++ Formula/sqs_replay.rb
@@ -1,9 +1,9 @@
 class SqsReplay < Formula
   desc "Replay SQS messages e.g. from a dead letter queue to the main queue"
   homepage "https://github.com/cdimascio/sqs-replay"
-  url "https://github.com/cdimascio/sqs-replay/releases/download/v0.10.0/sqs-replay-mac.tar.gz"
-  sha256 "db913863e852033f91fb6df59770c33bf9df52fe04083c681926cf2d08f0d8f6"
-  version "0.10.0"
+  url "https://github.com/cdimascio/sqs-replay/releases/download/0.12.0/sqs-replay-mac.tar.gz"
+  sha256 "0aacdc0e1287998de9d8ac275df4f6babf0429dd76ef9b4de86b8e0e01cda2dd"
+  version "0.12.0"
 
   def install
     bin.install "sqs-replay"
